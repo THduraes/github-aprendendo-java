d3cb2925f35a5f5514d529c3c80b60ee4e82f2c5
ThiagoDuraes <thiagosantos881@hotmail.com>
2025-01-02 14:39:24 -0300

    minha versao antes da correcao

---
+++ exercFixSec9/src/Aplication/Banco.java
@@ -0,0 +1,39 @@
+package Aplication;
+import java.util.Random;
+
+public class Banco {
+    private String name;
+    private int numberCount;
+    private float balance;
+
+    public Banco(){
+
+    }
+
+    public Banco(String name, int numberCount, float balance){
+        this.name = name;
+        this.numberCount = numberCount;
+        this.balance = balance;
+    }
+    public Banco(String name, int numberCount){
+        this.name = name;
+        this.numberCount = numberCount;
+    }
+
+    public void getDate(){
+        System.out.println("--ACCOUNT INFO--\nAccount owner: "+this.name+"\nAccount number: "+this.numberCount+
+                "\nAccount balance: "+this.balance+"\n");
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void addBalance(float balance){
+        this.balance += balance;
+        System.out.println("Deposit made successfully!\n");
+    }
+    public void delBalance(float balance){
+        this.balance -= balance+5;
+        System.out.println("Sake made successfully!\n");
+    }
+
+}
--- exercFixSec9/src/Main.java
+++ exercFixSec9/src/Main.java
@@ -1,15 +1,66 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Aplication.Banco;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Banco account = new Banco();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----WELCOME TO THE BANK----\n\n");
+        System.out.println("REGISTER YOUR ACCOUNT...\n");
+        System.out.printf("Name of owner account: ");
+        String name = sc.nextLine();
+        System.out.printf("Number account: ");
+        int numberAccount = sc.nextInt();
+        System.out.printf("Do you wish deposit money initially?\n1-Yes\n2-No\n\nOption: ");
+        int op = sc.nextInt();
+        if(op==1){
+            System.out.printf("How much: ");
+            float balance = sc.nextFloat();
+            account = new Banco(name, numberAccount, balance);
+            System.out.println("Register sucessful!\n\n");
+        } else if (op==2) {
+            System.out.println("Your balance initially is 0,0$");
+            account = new Banco(name, numberAccount);
+            System.out.println("Register sucessful!\n\n");
+        }else{
+            System.out.println("Option invalid.");
+            System.exit(0);
         }
+        System.out.println("--Menu Options--");
+        do {
+
+            System.out.printf("1-Perform deposity\n2-Make a withdrawal" +
+                    "\n3-Change name the account\n4-Exit program\nOption:");
+            op = sc.nextInt();
+            switch (op){
+                case 1:
+                    System.out.printf("How much: ");
+                    account.addBalance(sc.nextFloat());
+                    account.getDate();
+                    break;
+
+                case 2:
+                    System.out.printf("How much: ");
+                    account.delBalance(sc.nextFloat());
+                    account.getDate();
+                    break;
+
+                case 3:
+                    sc.nextLine();
+                    System.out.printf("What a new name: ");
+                    account.setName(sc.nextLine());
+                    account.getDate();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+                    break;
+            }
+        }while (op!=4);
     }
 }
